50916baa9fcbc5ddef8af189372e2b75fcb9ad35
angelashamonina <76558153+angelashamonina@users.noreply.github.com>
2021-12-18 19:49:00 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,46 @@
-// реализация пользовательского приложения
+#include <arithmetic.h>
 
-int main()
-{
-  return 0;
-}
+	using namespace std;
+	
+	int main()
+	{
+		try {
+			string s;
+			unsigned k;
+			cout << "Enter the expression " << endl;
+			getline(cin, s);
+			Arithmetic res;
+			res.vectorLexem(s);
+			cout << "Did you want to use some variables? " << endl;
+			cout << "1) Yes" << endl;
+			cout << "2) No" << endl;
+			cin >> k;
+			switch (k)
+			{
+			case 1:
+			{
+				res.PutVariable(res);
+				break;
+			}
+			case 2:
+			{
+				break;
+			}
+			}
+			if (res.Correct()) {
+				cout << endl;
+				res.Postfix();
+				res.print();
+				cout << endl;
+				cout << "Result: " << res.calculation();
+				cout << endl;
+			}
+		}
+	    catch (char* error) 
+		{
+					cout << error << endl;
+				}
+				cout << endl;
+		return 0;
+	}
+
